b42dedcd8944c4c103c72b8fc6d62dfda4e70518
Dillon Hafer <dh@dillonhafer.com>
2015-01-26 13:13:06 -0500

    Removes defual cache key generator

--- app/helpers/departments_helper.rb
+++ app/helpers/departments_helper.rb
@@ -1,13 +1,7 @@
 module DepartmentsHelper
-  def cache_key_for_products
-    count          = Product.count
-    max_updated_at = Product.maximum(:updated_at).try(:utc).try(:to_s, :number)
-    "products/all-#{count}-#{max_updated_at}"
-  end
-
-  def bia_cache_key_for(type)
-    count          = @department.send(type).count
-    max_updated_at = @department.send(type).maximum(:updated_at).try(:utc).try(:to_s, :number)
-    "dept/#{@department.id}/#{type}/all-#{count}-#{max_updated_at}"
+  def bia_cache_key_for(relationship)
+    count          = @department.send(relationship).count
+    max_updated_at = @department.send(relationship).maximum(:updated_at).try(:utc).try(:to_s, :number)
+    "dept/#{@department.id}/#{relationship}/all-#{count}-#{max_updated_at}"
   end
 end
