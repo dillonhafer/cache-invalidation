2e83af63c21a6f496df7a5382642491958ab80da
Dillon Hafer <dh@dillonhafer.com>
2015-01-28 13:13:16 -0500

    Adds proper schema and seeds

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150126160730) do
+ActiveRecord::Schema.define(version: 20150127203541) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,8 +25,15 @@ ActiveRecord::Schema.define(version: 20150126160730) do
   create_table "expenses", force: :cascade do |t|
     t.string   "name"
     t.integer  "department_id"
+    t.integer  "location_id"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+  end
+
+  create_table "locations", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "members", force: :cascade do |t|
