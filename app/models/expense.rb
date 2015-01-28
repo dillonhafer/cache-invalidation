28fe4d92a0b1f313b06945bf2ed8b760122019c0
Dillon Hafer <dh@dillonhafer.com>
2015-01-28 13:40:15 -0500

    Adds collection cache key override

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,3 +1,11 @@
 class Expense < ActiveRecord::Base
   belongs_to :department
+  belongs_to :location
+
+  def self.collection_cache_key
+    latest         = order('updated_at DESC').take
+    max_updated_at = [latest.updated_at, latest.location.updated_at].max
+                     .try(:utc).try(:to_s, :number)
+    [count,max_updated_at].join('-')
+  end
 end
